01860e906f1d0ea32010701c8c9e90c4ac603048
lynnniemeier <l.niemeier@student.rug.nl>
2020-09-24 17:18:04 +0200

    Commit Tutorial3.py

---
+++ Week1/Hello world.py
@@ -0,0 +1,16 @@
+print("Hello World")
+#Excercise 2.14
+#1.
+A="All"
+w="work"
+a="and"
+n="no"
+p="play"
+m="makes"
+J="Jack"
+o="a"
+d="dull"
+b="boy"
+.="."
+Print(A,w,a,n,p,m,J,o,d,b)
+
---
+++ Week2/Excersise 1.py
@@ -0,0 +1,13 @@
+# Excercise 2.14
+# 1.
+A="All"
+w="work"
+a="and"
+n="no"
+p="play"
+m="makes"
+J="Jack"
+o="a"
+d="dull"
+b="boy"
+print(A,w,a,n,p,m,J,o,d,b)
